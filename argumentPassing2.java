cc021edac649aec7de223318ffccac2d373ebd8f
Sakshi Borkar <166425641+SakshiBorkarSB@users.noreply.github.com>
2026-02-17 21:38:11 +0530

    Create argumentPassing2.java

---
+++ argumentPassing2.java
@@ -0,0 +1,20 @@
+// Call by Value
+
+import java.util.ArrayList;
+
+public class argumentPassing2 {
+    public static void main(String args[]) {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(2);
+        myList.add(4);
+        System.out.println("Before changing: " + myList);
+        change(myList);
+        System.out.println("After changing: " + myList);
+    }
+
+    public static void change(ArrayList<Integer> myList) {
+        myList = new ArrayList<>();
+        myList.add(3);
+        myList.add(5);
+    }
+}
